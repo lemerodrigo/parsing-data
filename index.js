d66eaa23901b6617f686b3567bec6376508e1dcc
Guilherme Davi Gomes de Lima <guiionfire@hotmail.com>
2018-11-29 10:03:35 -0200

    almost done

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ const data = require('./data.js');
 // Returns an array of titles that are not in English
 // field: language
 function getAllNonEnglishTitles() {
-
+    let titles = [];
+    data.forEach(obj => {
+        if (obj.language !== "English") {
+            titles.push(obj.movie_title)
+        }
+    });
+    return titles;
 }
 
 // Returns an array with title names of titles
 // that lasts more than 2 hours and a half.
 // field: duration
 function titlesWithMoreThan2HoursAndAHalf() {
-
+    let titles = []
+    data.forEach(obj => {
+        if (obj.duration > 150) {
+            titles.push(obj.movie_title)
+        }
+    });
+    return titles;
 }
 
 // Returns an array of all Countries that have produced titles.
@@ -28,5 +40,11 @@ function topTenTitles() {
 // Returns the number of titles that were launched in 2015.
 // field: title_year
 function titlesFrom2015() {
-  
+    let count = 0;
+    data.forEach(obj => {
+        if(obj.title_year === 2015) {
+            count += 1
+        }
+    })
+    return count;
 }
