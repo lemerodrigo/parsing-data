7c55b03d1554bfe1e10be4cfbbea772818174d25
Rodrigo Leme de Mello <rodrigo@kepler.local>
2018-11-28 23:16:51 -0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const data = require('./data.js');
+
+// Returns an array of titles that are not in English
+// field: language
+function getAllNonEnglishTitles() {
+
+}
+
+// Returns an array with title names of titles
+// that lasts more than 2 hours and a half.
+// field: duration
+function titlesWithMoreThan2HoursAndAHalf() {
+
+}
+
+// Returns an array of all Countries that have produced titles.
+// field: country
+function allCountries() {
+
+}
+
+// Returns an array of titles name with the top ten titles
+// field: imdb_score
+function topTenTitles() {
+
+}
+
+// Returns the number of titles that were launched in 2015.
+// field: title_year
+function titlesFrom2015() {
+  
+}
